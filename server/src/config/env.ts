411ef2979f929a773526857c3134d20080fe1b90
roopsagar-k <roopsagarudayar9854@gmail.com>
2025-06-08 08:26:59 +0530

    environment config update

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -5,10 +5,9 @@ dotenv.config();
 export const ENV = {
   FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID!,
   FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL!,
-  FIREBASE_PRIVATE_KEY: (process.env.FIREBASE_PRIVATE_KEY || "").replace(
-    /\\n/g,
-    "\n"
-  ),
+  FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY?.includes("\\n")
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : process.env.FIREBASE_PRIVATE_KEY,
   JWT_SECRET: (process.env.JWT_SECRET || "").toString(),
   CLIENT_URL: (process.env.CLIENT_URL || "").toString(),
 };
